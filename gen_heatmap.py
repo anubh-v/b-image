75f619042142914e30af89c7271abd15c62aa0ea
anubh-v <anubhav@u.nus.edu>
2019-08-08 13:11:51 +0800

    Fix typo

--- gen_heatmap.py
+++ gen_heatmap.py
@@ -7,5 +7,5 @@ def gen_heatmap(data, title):
   fig = plot.get_figure()
   fig.savefig(title)
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
   gen_heatmap([[0, 1, 2, 3, 4, 5],[1, 2, 3, 1, 1, 1]])
