22ac14a07c23651f24726725facef45f9c5af8f4
anubh-v <anubhav@u.nus.edu>
2019-08-08 13:18:14 +0800

    Improve organisation

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from gen_heatmap import gen_heatmap
 import sys
 
-if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    print("Usage: python main.py [input_binary] [pic_title]")
-    sys.exit(1)
-  
-  input_binary = sys.argv[1]
-  pic_title = sys.argv[2]
-  read_and_generate(input_binary, pic_title)
-
 def read_and_generate(input_binary, pic_title):
   
   # Initialise 2D representation
@@ -33,3 +24,13 @@ def read_and_generate(input_binary, pic_title):
   
   # Generate heatmap using 2D representation
   gen_heatmap(counter, sys.argv[2])
+
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print("Usage: python main.py [input_binary] [pic_title]")
+    sys.exit(1)
+  
+  input_binary = sys.argv[1]
+  pic_title = sys.argv[2]
+  read_and_generate(input_binary, pic_title)
+
