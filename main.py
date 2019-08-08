381677390b95c5c1f79bed5b2a34cc6463f19343
anubh-v <anubhav@u.nus.edu>
2019-08-08 13:12:45 +0800

    Improve organisation

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 from gen_heatmap import gen_heatmap
-
 import sys
 
-input_file = sys.argv[1]
-
-counter = []
-for i in range(256):
-  lst = []
-  for j in range(256):
-    lst.append(0)
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print("Usage: python main.py [input_binary] [pic_title]")
+    sys.exit(1)
   
-  counter.append(lst)
+  input_binary = sys.argv[1]
+  pic_title = sys.argv[2]
 
-with open(input_file, 'rb') as bin_file:
-  byte = bin_file.read(2)
-  while byte:
-    if len(byte) == 2:
-      b, c = byte 
-      counter[b][c] += 1
+def read_and_generate(input_binary, pic_title):
+  
+  # Initialise 2D representation
+  counter = []
+  for i in range(256):
+    lst = []
+    for j in range(256):
+      lst.append(0)
+  
+    counter.append(lst)
+
+  # Fill 2D representation
+  with open(input_file, 'rb') as bin_file:
+    byte = bin_file.read(2)
+    while byte:
+      if len(byte) == 2:
+        b, c = byte 
+        counter[b][c] += 1
 
     byte = bin_file.read(2)
-
-gen_heatmap(counter, sys.argv[2])
+  
+  # Generate heatmap using 2D representation
+  gen_heatmap(counter, sys.argv[2])
